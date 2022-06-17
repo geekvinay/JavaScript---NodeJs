9ffcefaa8ef320d0b404caae2b63031d07c6ffa3
geekvinay <geekvinay002@gmail.com>
2022-06-17 16:22:16 +0530

    Learning about jwt

---
+++ playground/promise-chaining.js
@@ -0,0 +1,18 @@
+// const doWork = async () => {
+//   const sum = await add(1, 99);
+//   return sum;
+// };
+
+// const add = async (a, b) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(a + b);
+//     }, 2000);
+//   });
+// };
+
+// doWork()
+//   .then((res) => {
+//     console.log("Result", res);
+//   })
+//   .catch((e) => console.log(e));
